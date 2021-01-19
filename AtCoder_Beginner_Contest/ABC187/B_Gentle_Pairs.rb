fb3c17320866393de23fd12b6655f6cef3d6ad9a
RyotaST <omamesan0621@icloud.com>
2021-01-20 01:22:14 +0900

    Gentle_Pairs-解答

--- AtCoder_Beginner_Contest/ABC187/B_Gentle_Pairs.rb
+++ AtCoder_Beginner_Contest/ABC187/B_Gentle_Pairs.rb
@@ -1,14 +1,16 @@
 box = []
 sum = 0
+count = 0
 N = gets.chomp.to_i
 N.times {box.push(gets.chomp.split.map(&:to_i))}
 
 if N > 1
   (N-1).downto(1) do |n1|
     n1.times do |n2|
-      slope = ((box[n2][1] - box[n2+1][1]).rationalize / (box[n2][0] - box[n2+1][0]).rationalize).to_f
+      slope = (box[count][1] - box[n2+count+1][1]).rationalize / (box[count][0] - box[n2+count+1][0]).rationalize
       sum += 1 if slope.between?(-1, 1)
     end
+    count += 1
   end
 end
 
