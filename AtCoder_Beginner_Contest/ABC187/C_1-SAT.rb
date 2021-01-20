aa4ae8c58acc50dafe5e7e7a06c1040fc2b9a192
RyotaST <omamesan0621@icloud.com>
2021-01-21 07:17:36 +0900

    1_SAT-タイムオーバー2

--- AtCoder_Beginner_Contest/ABC187/C_1-SAT.rb
+++ AtCoder_Beginner_Contest/ABC187/C_1-SAT.rb
@@ -5,19 +5,10 @@ result = 'satisfiable'
 
 N.times do |n1|
   if result == 'satisfiable'
-    if strings[n1].include?('!')
-      (n1+1).upto(strings.size) do |n2|
-        if strings[n2] == (strings[n1][1..-1])
-          result = strings[n2]
-          break
-        end
-      end
-    else
-      (n1+1).upto(strings.size) do |n2|
-        if strings[n2] == "!#{strings[n1]}"
-          result = strings[n1]
-          break
-        end
+    if strings[n1][0] == '!'
+      if strings.include?(strings[n1][1..-1])
+        result = strings[n1][1..-1]
+        break
       end
     end
   else
