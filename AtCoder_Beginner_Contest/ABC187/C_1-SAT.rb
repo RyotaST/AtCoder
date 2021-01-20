3fe1ee22a2ea6934cc09f1c3d3a6c653f2f87bcb
RyotaST <omamesan0621@icloud.com>
2021-01-21 08:10:58 +0900

    1_SAT-タイムオーバー

--- AtCoder_Beginner_Contest/ABC187/C_1-SAT.rb
+++ AtCoder_Beginner_Contest/ABC187/C_1-SAT.rb
@@ -1,18 +1,41 @@
 N = gets.chomp.to_i
-strings = []
-N.times {strings.push(gets.chomp)}
+strings_box = []
+mark = []
+N.times do
+  string = gets.chomp
+  if string[0] == '!'
+    if mark.include?(string)
+      next
+    else
+      mark << string
+    end
+  else
+    if strings_box.include?(string)
+      next
+    else
+      strings_box << string
+    end
+  end
+end
 result = 'satisfiable'
 
-N.times do |n1|
-  if result == 'satisfiable'
-    if strings[n1][0] == '!'
-      if strings.include?(strings[n1][1..-1])
-        result = strings[n1][1..-1]
+if mark.size < strings_box.size
+  mark.size.times do |n1|
+    if result == 'satisfiable'
+      if strings_box.include?(mark[n1][1..-1])
+        result = mark[n1][1..-1]
         break
       end
     end
-  else
-    break
+  end
+else
+  strings_box.size.times do |n2|
+    if result == 'satisfiable'
+      if mark.include?("!#{strings_box[n2]}")
+        result = strings_box[n2]
+        break
+      end
+    end
   end
 end
 
