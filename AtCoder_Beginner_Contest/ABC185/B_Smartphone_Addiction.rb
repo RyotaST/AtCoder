ad4c597042420e53761e5dda5293b8c88b33124b
RyotaST <omamesan0621@icloud.com>
2021-01-26 00:11:23 +0900

    Smartphone_Addiction-解答

--- AtCoder_Beginner_Contest/ABC185/B_Smartphone_Addiction.rb
+++ AtCoder_Beginner_Contest/ABC185/B_Smartphone_Addiction.rb
@@ -1,8 +1,25 @@
 N, M, T = gets.split.map(&:to_i)
-cafe.times = []
+cafe_times = []
 battery = N
+result = 'Yes'
 M.times{cafe_times.push(gets.split.map(&:to_i))}
 
-cafe_times.each_cons(1) do |n|
-  p n
+battery = N - cafe_times[0][0]
+result = 'No' if battery <= 0
+if M > 1
+  cafe_times.each_cons(2) do |n|
+    battery += n[0][1] - n[0][0]
+    battery = N if battery > N
+    battery -= n[1][0] - n[0][1]
+    if battery <= 0
+      result = 'No'
+      break
+    end
+  end
 end
+battery += cafe_times[-1][1] - cafe_times[-1][0]
+battery = N if battery > N
+battery -= T - cafe_times[-1][1]
+result = 'No' if battery <= 0
+
+puts result
