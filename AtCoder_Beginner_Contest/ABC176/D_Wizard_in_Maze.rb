803c8ab0e53722cf58cc5ce5ec3e524fc8f437d3
RyotaST <omamesan0621@icloud.com>
2021-02-13 19:20:08 +0900

    Wizard in Maze-通らん

--- AtCoder_Beginner_Contest/ABC176/D_Wizard_in_Maze.rb
+++ AtCoder_Beginner_Contest/ABC176/D_Wizard_in_Maze.rb
@@ -1,52 +1,47 @@
-require 'thread'
-
 H, W = gets.split.map(&:to_i)
 C = gets.split.map(&:to_i)
 D = gets.split.map(&:to_i)
-field = []
-H.times do
-  field.push(gets.chomp.chars)
+field = Array.new(H+4){Array.new}
+(H+4).times do |n|
+  if (n==0)||(n==1)||(n==H+2)||(n==H+3)
+    (W+4).times{field[n].push("#")}
+  else
+    field[n] = ("##"+gets.chomp+"##").chars
+  end
 end
-dx2 = [2, 2, 2,  2,  2, 1, 1, 1,  1,  1, 0, 0,  0,  0, -1, -1, -1, -1, -1, -2, -2, -2, -2, -2]
-dy2 = [2, 1, 0, -1, -2, 2, 1, 0, -1, -2, 2, 1, -1, -2,  2,  1,  0, -1, -2,  2,  1,  0, -1, -2]
+dx2 = [2, 2, 2,  2,  2, 1, 1,  1,  1, 0,  0, -1, -1, -1, -1, -2, -2, -2, -2, -2]
+dy2 = [2, 1, 0, -1, -2, 2, 1, -1, -2, 2, -2,  2,  1, -1, -2,  2,  1,  0, -1, -2]
 
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-graph1 = Array.new(H) {Array.new(W) {Array.new}}
-graph2 = Array.new(H) {Array.new(W) {Array.new}}
+dist = Array.new(H+4){Array.new(W+4){-1}}
+que = [[],[]]
+dist[C[0]+1][C[1]+1] = 0
+que[0].push([C[0]+1,C[1]+1])
 
-dist = Array.new(H){Array.new(W){-1}}
-que = Queue.new
-que2 = Queue.new
-dist[C[0]-1][C[1]-1] = 0
-que.push([C[0]-1,C[1]-1])
-que2.push([C[0]-1,C[1]-1])
-
-until que.empty? && que2.empty?
-  if !(que.empty?)
-    v = que.pop
-    que2.push(v)
+until que[0].empty? && que[1].empty?
+  if !(que[0].empty?)
+    v = que[0].shift
+    que[1].push(v)
     0.upto(3) do |n|
-      next if (v[0]+dx[n] < 0) || (v[0]+dx[n] > H-1) || (v[1]+dy[n] < 0) || (v[1]+dy[n] > W-1)
       next if field[v[0]+dx[n]][v[1]+dy[n]] == '#'
       next if dist[v[0]+dx[n]][v[1]+dy[n]] != -1
       dist[v[0]+dx[n]][v[1]+dy[n]] = dist[v[0]][v[1]]
-      que.push([v[0]+dx[n], v[1]+dy[n]])
-      que2.push([v[0]+dx[n], v[1]+dy[n]])
+      que[0].push([v[0]+dx[n], v[1]+dy[n]])
+      que[1].push([v[0]+dx[n], v[1]+dy[n]])
     end
   else
-    until que2.empty?
-      x = que2.pop
-      0.upto(23) do |n1|
-        next if (x[0]+dx2[n1] < 0) || (x[0]+dx2[n1] > H-1) || (x[1]+dy2[n1] < 0) || (x[1]+dy2[n1] > W-1)
+    until que[1].empty?
+      x = que[1].shift
+      0.upto(19) do |n1|
         next if field[x[0]+dx2[n1]][x[1]+dy2[n1]] == '#'
         next if dist[x[0]+dx2[n1]][x[1]+dy2[n1]] != -1
         dist[x[0]+dx2[n1]][x[1]+dy2[n1]] = dist[x[0]][x[1]] + 1
-        que.push([x[0]+dx2[n1], x[1]+dy2[n1]])
+        que[0].push([x[0]+dx2[n1], x[1]+dy2[n1]])
       end
     end
   end
 end
 
-p dist[D[0]-1][D[1]-1]
+p dist[D[0]+1][D[1]+1]
