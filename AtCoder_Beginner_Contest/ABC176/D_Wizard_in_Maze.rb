8943569ef7dbc37e473197d1a757540d72f05d41
RyotaST <omamesan0621@icloud.com>
2021-02-13 17:21:52 +0900

    Wizard in Maze-TLE残り1

--- AtCoder_Beginner_Contest/ABC176/D_Wizard_in_Maze.rb
+++ AtCoder_Beginner_Contest/ABC176/D_Wizard_in_Maze.rb
@@ -7,163 +7,44 @@ field = []
 H.times do
   field.push(gets.chomp.chars)
 end
-count = 0
+dx2 = [2, 2, 2,  2,  2, 1, 1, 1,  1,  1, 0, 0,  0,  0, -1, -1, -1, -1, -1, -2, -2, -2, -2, -2]
+dy2 = [2, 1, 0, -1, -2, 2, 1, 0, -1, -2, 2, 1, -1, -2,  2,  1,  0, -1, -2,  2,  1,  0, -1, -2]
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
 graph1 = Array.new(H) {Array.new(W) {Array.new}}
 graph2 = Array.new(H) {Array.new(W) {Array.new}}
-0.upto(H-1) do |n1|
-  0.upto(W-1) do |n2|
-    next if field[n1][n2] == '#'
-    if n1+2 <= H-1 && n2+2 <=W-1
-      if field[n1+2][n2+2] != '#'
-        graph2[n1][n2].push([n1+2,n2+2])
-      end
-    end
-    if n1+2 <= H-1 && n2+1 <=W-1
-      if field[n1+2][n2+1] != '#'
-        graph2[n1][n2].push([n1+2,n2+1])
-      end
-    end
-    if n1+2 <= H-1
-      if field[n1+2][n2] != '#'
-        graph2[n1][n2].push([n1+2,n2])
-      end
-    end
-    if n1+2 <= H-1 && n2-1 >= 0
-      if field[n1+2][n2-1] != '#'
-        graph2[n1][n2].push([n1+2,n2-1])
-      end
-    end
-    if n1+2 <= H-1 && n2-2 >= 0
-      if field[n1+2][n2-2] != '#'
-        graph2[n1][n2].push([n1+2,n2-2])
-      end
-    end
-    if n1+1 <= H-1 && n2+2 <=W-1
-      if field[n1+1][n2+2] != '#'
-        graph2[n1][n2].push([n1+1,n2+2])
-      end
-    end
-    if n1+1 <= H-1 && n2+1 <=W-1
-      if field[n1+1][n2+1] != '#'
-        graph2[n1][n2].push([n1+1,n2+1])
-      end
-    end
-    if n1+1 <= H-1
-      if field[n1+1][n2] != '#'
-        graph1[n1][n2].push([n1+1,n2])
-      end
-    end
-    if n1+1 <= H-1 && n2-1 >= 0
-      if field[n1+1][n2-1] != '#'
-        graph2[n1][n2].push([n1+1,n2-1])
-      end
-    end
-    if n1+1 <= H-1 && n2-2 >= 0
-      if field[n1+1][n2-2] != '#'
-        graph2[n1][n2].push([n1+1,n2-2])
-      end
-    end
-    if n2+2 <= W-1
-      if field[n1][n2+2] != '#'
-        graph2[n1][n2].push([n1,n2+2])
-      end
-    end
-    if n2+1 <= W-1
-      if field[n1][n2+1] != '#'
-        graph1[n1][n2].push([n1,n2+1])
-      end
-    end
-    if n2-1 >= 0
-      if field[n1][n2-1] != '#'
-        graph1[n1][n2].push([n1,n2-1])
-      end
-    end
-    if n2-2 >= 0
-      if field[n1][n2-2] != '#'
-        graph2[n1][n2].push([n1,n2-2])
-      end
-    end
-    if n1-1 >= 0 && n2+2 <= W-1
-      if field[n1-1][n2+2] != '#'
-        graph2[n1][n2].push([n1-1,n2+2])
-      end
-    end
-    if n1-1 >= 0 && n2+1 <= W-1
-      if field[n1-1][n2+1] != '#'
-        graph2[n1][n2].push([n1-1,n2+1])
-      end
-    end
-    if n1-1 >= 0
-      if field[n1-1][n2] != '#'
-        graph1[n1][n2].push([n1-1,n2])
-      end
-    end
-    if n1-1 >= 0 && n2-1 >= 0
-      if field[n1-1][n2-1] != '#'
-        graph2[n1][n2].push([n1-1,n2-1])
-      end
-    end
-    if n1-1 >= 0 && n2-2 >= 0
-      if field[n1-1][n2-2] != '#'
-        graph2[n1][n2].push([n1-1,n2-2])
-      end
-    end
-    if n1-2 >= 0 && n2+2 <= W-1
-      if field[n1-2][n2+2] != '#'
-        graph2[n1][n2].push([n1-2,n2+2])
-      end
-    end
-    if n1-2 >= 0 && n2+1 <= W-1
-      if field[n1-2][n2+1] != '#'
-        graph2[n1][n2].push([n1-2,n2+1])
-      end
-    end
-    if n1-2 >= 0
-      if field[n1-2][n2] != '#'
-        graph2[n1][n2].push([n1-2,n2])
-      end
-    end
-    if n1-2 >= 0 && n2-1 >= 0
-      if field[n1-2][n2-1] != '#'
-        graph2[n1][n2].push([n1-2,n2-1])
-      end
-    end
-    if n1-2 >= 0 && n2-2 >= 0
-      if field[n1-2][n2-2] != '#'
-        graph2[n1][n2].push([n1-2,n2-2])
-      end
-    end
-  end
-end
 
 dist = Array.new(H){Array.new(W){-1}}
-que1 = Queue.new
+que = Queue.new
 que2 = Queue.new
-
 dist[C[0]-1][C[1]-1] = 0
-que1.push([C[0]-1,C[1]-1])
+que.push([C[0]-1,C[1]-1])
 que2.push([C[0]-1,C[1]-1])
 
-until que1.empty?
-  if !(que1.empty?)
-    v = que1.pop
-    if !(graph1[v[0]][v[1]].empty?)
-      graph1[v[0]][v[1]].each do |nv|
-        next if dist[nv[0]][nv[1]] != -1
-        dist[nv[0]][nv[1]] = dist[v[0]][v[1]]
-        que1.push(nv)
-        que2.push(nv)
-      end
-    else
-      que2.push(v)
+until que.empty? && que2.empty?
+  if !(que.empty?)
+    v = que.pop
+    que2.push(v)
+    0.upto(3) do |n|
+      next if (v[0]+dx[n] < 0) || (v[0]+dx[n] > H-1) || (v[1]+dy[n] < 0) || (v[1]+dy[n] > W-1)
+      next if field[v[0]+dx[n]][v[1]+dy[n]] == '#'
+      next if dist[v[0]+dx[n]][v[1]+dy[n]] != -1
+      dist[v[0]+dx[n]][v[1]+dy[n]] = dist[v[0]][v[1]]
+      que.push([v[0]+dx[n], v[1]+dy[n]])
+      que2.push([v[0]+dx[n], v[1]+dy[n]])
     end
   else
-    x = que2.pop
-    graph2[x[0]][x[1]].each do |xv|
-      next if dist[xv[0]][xv[1]] != -1
-      dist[xv[0]][xv[1]] = dist[x[0]][x[1]]+1
-      que1.push(xv)
+    until que2.empty?
+      x = que2.pop
+      0.upto(23) do |n1|
+        next if (x[0]+dx2[n1] < 0) || (x[0]+dx2[n1] > H-1) || (x[1]+dy2[n1] < 0) || (x[1]+dy2[n1] > W-1)
+        next if field[x[0]+dx2[n1]][x[1]+dy2[n1]] == '#'
+        next if dist[x[0]+dx2[n1]][x[1]+dy2[n1]] != -1
+        dist[x[0]+dx2[n1]][x[1]+dy2[n1]] = dist[x[0]][x[1]] + 1
+        que.push([x[0]+dx2[n1], x[1]+dy2[n1]])
+      end
     end
   end
 end
