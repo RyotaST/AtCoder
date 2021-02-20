2cedefad1469bc81568ab6743e7e7a02feb55afd
RyotaST <omamesan0621@icloud.com>
2021-02-20 23:34:49 +0900

    Base n-作成

---
+++ AtCoder_Beginner_Contest/ABC192/D_Base_n.rb
@@ -0,0 +1,27 @@
+X = gets.chomp.to_s
+x = X.to_s.chars.map(&:to_i)
+M = gets.to_i
+count = 0
+
+def conversion(x, a)
+  b = 0
+  result = 0
+  x.each do |n|
+    b += 1
+    result += n * (a ** (x.length-b))
+  end
+  return result
+end
+
+a = x.max
+
+while true
+  a = a+1
+  if conversion(x, a) <= M
+    count+=1
+  else
+    break
+  end
+end
+
+puts count
