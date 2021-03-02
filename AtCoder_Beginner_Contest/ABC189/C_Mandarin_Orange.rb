6cdcc48b571eec313a88874f76db1039f8517861
RyotaST <omamesan0621@icloud.com>
2021-03-02 23:07:47 +0900

    Manderin Orange-解答

--- AtCoder_Beginner_Contest/ABC189/C_Mandarin_Orange.rb
+++ AtCoder_Beginner_Contest/ABC189/C_Mandarin_Orange.rb
@@ -1,16 +1,13 @@
-N = gets.to_i
+n = gets.to_i
 a = gets.split.map(&:to_i)
-box = []
-sum = 0
-result = 0
-l = 0
-r = 0
-x = 0
 result = 0
 
-1.upto(a.size) do |n1|
-  a.each_cons(n1) do |n2|
-    result = n2.min * n2.size if n2.min * n2.size > result
+n.times do |n1|
+  min = a[n1]
+  n1.upto(n-1) do |n2|
+    min = a[n2] if min > a[n2]
+    break if min * n-n1 < result
+    result = min * (n2-n1+1) if min * (n2-n1+1) > result
   end
 end
 
