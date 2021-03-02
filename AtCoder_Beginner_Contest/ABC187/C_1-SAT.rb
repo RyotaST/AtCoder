a2b616dd69bea8758c13024e722fdbc497fd4758
RyotaST <omamesan0621@icloud.com>
2021-03-03 00:45:45 +0900

    1-SAT-解答

--- AtCoder_Beginner_Contest/ABC187/C_1-SAT.rb
+++ AtCoder_Beginner_Contest/ABC187/C_1-SAT.rb
@@ -1,42 +1,16 @@
 N = gets.chomp.to_i
-strings_box = []
-mark = []
-N.times do
-  string = gets.chomp
-  if string[0] == '!'
-    if mark.include?(string)
-      next
-    else
-      mark << string
-    end
+strings1 = []
+strings2 = []
+
+N.times do |n|
+  n = gets.chomp
+  if n[0] == "!"
+    strings1 << n[1..-1]
   else
-    if strings_box.include?(string)
-      next
-    else
-      strings_box << string
-    end
-  end
-end
-result = 'satisfiable'
-
-if mark.size < strings_box.size
-  mark.size.times do |n1|
-    if result == 'satisfiable'
-      if strings_box.include?(mark[n1][1..-1])
-        result = mark[n1][1..-1]
-        break
-      end
-    end
-  end
-else
-  strings_box.size.times do |n2|
-    if result == 'satisfiable'
-      if mark.include?("!#{strings_box[n2]}")
-        result = strings_box[n2]
-        break
-      end
-    end
+    strings2 << n
   end
 end
 
-puts result
+sum = strings1 & strings2
+
+puts sum.empty? ? 'satisfiable' : sum[0]
