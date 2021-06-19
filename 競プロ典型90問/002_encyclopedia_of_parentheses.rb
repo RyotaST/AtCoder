520f2d0c39adb6daac7a9cbfed68130b4da8cdec
RyotaST <omamesan0621@icloud.com>
2021-06-20 03:32:58 +0900

    002 解答

--- 競プロ典型90問/002_encyclopedia_of_parentheses.rb
+++ 競プロ典型90問/002_encyclopedia_of_parentheses.rb
@@ -1,7 +1,12 @@
 N = gets.to_i
-strings = Array.new
-(N/2).times do
-  strings << "(" << ")"
+count = 0
+result = []
+["(", ")"].repeated_permutation(N).to_a.each do |n|
+  n.each do |value|
+    (value == "(") ? (count += 1) : (count -= 1)
+    break if count < 0 || count > N/2
+  end
+  result.push(n.join) if count == 0
+  count = 0
 end
-strings.sort!
-p strings
+result.sort.each{|n| puts n}
