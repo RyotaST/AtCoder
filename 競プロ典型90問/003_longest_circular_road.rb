bbda7f37718ef4c58484421d8fb1fa4a52c7114c
RyotaST <omamesan0621@icloud.com>
2021-06-20 01:33:12 +0900

    003 AC(最長ルートの末端を始点へ)

--- 競プロ典型90問/003_longest_circular_road.rb
+++ 競プロ典型90問/003_longest_circular_road.rb
@@ -11,11 +11,10 @@ count = 0
 check = Array.new(N){-1}
 queue = [[1]]
 judge = false
-p route
+tip = 0
 
 until queue.empty?
   num = []
-  p queue, count
   x = queue.shift
   x.each do |a|
     check[a-1] = count
@@ -27,6 +26,31 @@ until queue.empty?
         judge = true
       end
     end
+    tip = a if judge == false
+  end
+  queue.push(num) if !(num.empty?)
+  count += 1 if judge == true
+  judge = false
+end
+
+check = Array.new(N){-1}
+queue = [[tip]]
+count = 0
+
+until queue.empty?
+  num = []
+  x = queue.shift
+  x.each do |a|
+    check[a-1] = count
+    route[a-1].each do |n1|
+      if check[n1-1] != -1
+        next
+      else
+        num.push(n1)
+        judge = true
+      end
+    end
+    tip = route[a-1] if judge == false
   end
   queue.push(num) if !(num.empty?)
   count += 1 if judge == true
