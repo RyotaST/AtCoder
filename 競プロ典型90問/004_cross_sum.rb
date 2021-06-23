f262047f402c4e1644be85e8785b2b74a0035d87
RyotaST <omamesan0621@icloud.com>
2021-06-24 00:37:29 +0900

    002 TLE

--- 競プロ典型90問/004_cross_sum.rb
+++ 競プロ典型90問/004_cross_sum.rb
@@ -1,5 +1,11 @@
 H,W = gets.chomp.split.map(&:to_i)
 box = Array.new(H){gets.chomp.split.map(&:to_i)}
 rows_sum = []
-columns_sum = []
-box.each{|n| rows_sum.push(n.inject{|result, item| result+item})}
+columns_sum = Array.new(W){0}
+box.each do |n|
+  rows_sum.push(n.inject{|result, item| result+item})
+  W.times{|time| columns_sum = columns_sum.map{|x|} x+box[time]}
+end
+
+p rows_sum
+p columns_sum
