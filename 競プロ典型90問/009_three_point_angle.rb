51c0e696c03f158528b558d31465a8965e9cef35
RyotaST <omamesan0621@icloud.com>
2021-06-26 01:12:09 +0900

    009 最大の辺を算出

--- 競プロ典型90問/009_three_point_angle.rb
+++ 競プロ典型90問/009_three_point_angle.rb
@@ -1,1 +1,9 @@
 p = Array.new(gets.to_i){gets.split.map(&:to_i)}
+p.combination(3) do |x|
+  max = Math.sqrt((x[0][0]-x[1][0])^2 + (x[0][1]-x[1][1])^2)
+  if Math.sqrt((x[1][0]-x[2][0])^2 + (x[1][1]-x[2][1])^2) > max
+    max = Math.sqrt((x[1][0]-x[2][0])^2 + (x[1][1]-x[2][1])^2)
+  elsif Math.sqrt((x[2][0]-x[0][0])^2 + (x[2][1]-x[0][1])^2) > max
+    max = Math.sqrt((x[1][0]-x[2][0])^2 + (x[1][1]-x[2][1])^2)
+  end
+end
