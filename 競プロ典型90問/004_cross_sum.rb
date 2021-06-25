351821747cb70356a9ed047a1a2c63eb77887f4f
RyotaST <omamesan0621@icloud.com>
2021-06-26 01:11:09 +0900

    004 不必要な行を削除

--- 競プロ典型90問/004_cross_sum.rb
+++ 競プロ典型90問/004_cross_sum.rb
@@ -2,6 +2,7 @@ H,W = gets.chomp.split.map(&:to_i)
 box = Array.new(H){gets.chomp.split.map(&:to_i)}
 rows_sum = Array.new(H){0}
 columns_sum = Array.new(W){0}
+
 box.each_with_index do |value1, i1|
   columns_sum.each_with_index do |value2, i2|
     columns_sum[i2] += value1[i2]
@@ -10,11 +11,10 @@ box.each_with_index do |value1, i1|
 end
 
 result = Array.new(H){Array.new}
-rows_sum
-columns_sum
+
 rows_sum.each_with_index do |value1, i1|
   columns_sum.each_with_index do |value2, i2|
     result[i1].push(value1 + value2 - box[i1][i2])
-  end  
+  end
   puts result[i1].join(" ")
 end
