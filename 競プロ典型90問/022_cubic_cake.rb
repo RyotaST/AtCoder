6eca0c0b01f2fc1ad1ddc30497668265fe48a3c3
RyotaST <omamesan0621@icloud.com>
2021-07-11 01:19:36 +0900

    022 AC

--- 競プロ典型90問/022_cubic_cake.rb
+++ 競プロ典型90問/022_cubic_cake.rb
@@ -1,14 +1,3 @@
 A,B,C=gets.split.map(&:to_i)
 num = A.gcd(B).gcd(C)
-result = 0
-if A != num
-  result += (A/num-1)
-end
-if A != num
-  result += (B/num-1)
-end
-if C != num
-  result += (C/num-1)
-end
-
-puts result
+puts (A/num-1)+(B/num-1)+(C/num-1)
