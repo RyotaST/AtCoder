4b981ffd55b5b0f9a120af02c98ca513df8fb2e7
RyotaST <omamesan0621@icloud.com>
2021-07-21 01:19:03 +0900

    001 切れ目毎の長さ計算・条件分岐の修正

--- 競プロ典型90問/001_Yokan_Party.rb
+++ 競プロ典型90問/001_Yokan_Party.rb
@@ -1,26 +1,42 @@
+#input
 n,l = gets.split.map(&:to_i)
 k = gets.to_i
-a = gets.split.map(&:to_i)
-result = 0
-min = 0
-left = 0
+a1 = gets.split.map(&:to_i)
 
-answer = (1..l).bsearch do |x|
-  a.each_with_index do |value, index|
-    if index == 0
-      result = 0
-    end
-    result += value
-    if index == (a.length-1)
-      break
-    end
-    if result >= x
-      result = 0
-      next
-    end
-  end
-  puts result
-  result >= x
+#切れ目毎の長さをa2に格納
+a2 = []
+left = 0
+a1.each_with_index do |value, index|
+  index == 0 ? a2 << value : a2 << value-left
+  left = value
+  a2 << (l - left) if index == a1.length-1
 end
 
-puts answer
+def check(x, a2, k)
+  result = 0
+  count = 0
+  check = false
+  a2.each_with_index do |value, index|
+    result += value
+    if index == a2.length-1
+      break
+    end
+    if result >= -x
+      result = 0
+      count += 1
+    end
+    if count > k
+      check = true
+    end
+  end
+  if check == false
+  	(count == k) && (result >= -x) ? check = true : check = false
+  end
+  check
+end
+
+answer = (-l..-1).bsearch do |x|
+  check(x,a2,k)
+end
+
+puts -answer
