fb51cae94589614fb5fab12b631bf5cbeb1caea1
RyotaST <omamesan0621@icloud.com>
2023-06-27 07:58:23 +0900

    再帰で途中まで

---
+++ AtCoder_Reguler_Contest/Many_Fomulas.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fomulas(int num, int size){
+  if(size == 0){
+    return num;
+  }
+  else{
+    size -= 1;
+    for(int i=0; i<size; i++){
+      
+    }
+  }
+}
+
+int main() {
+  string S;
+  cin >> S;
+  vector<int> num(S.size());
+  cout << stoi(S);
+}
