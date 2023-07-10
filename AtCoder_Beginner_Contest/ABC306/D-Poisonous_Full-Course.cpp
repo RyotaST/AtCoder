0570d2a27afe5a5fc2b00e970b08a14827a23810
RyotaST <omamesan0621@icloud.com>
2023-07-10 12:21:18 +0900

    DPで途中まで

--- AtCoder_Beginner_Contest/ABC306/D-Poisonous_Full-Course.cpp
+++ AtCoder_Beginner_Contest/ABC306/D-Poisonous_Full-Course.cpp
@@ -4,76 +4,22 @@ using namespace std;
 int main(){
   int N;
   cin >> N;
-  int anti_count = 0;
-  int poison_count = 0;
-  int min_count;
-  int damage = 0;
-  long long ans = 0;
-  priority_queue<long long> dishes_anti;
-  priority_queue<long long> dishes_poison;
-  for(int i=0;i<N;i++){
-    int a;
-    long long b;
-    cin >> a >> b;
-    if(a == 1){
-      if(b <= 0){
-        continue;
-      }
-      dishes_poison.push(b);
-      anti_count += 1;
-    }
-    else{
-      dishes_anti.push(b);
-      poison_count += 1;
-    }
-  }
-  min_count = min(anti_count, poison_count);
-  while(1){
+  vector<vector<long long>>dp(N, vector<long long>(2));
+  vector<vector<long long>>value(N, vector<long long>(2));
+  dp.at(0).at(0) = 0;
+  for(int i=1;i<=N;i++){
     int a,b;
-    if(dishes_poison.empty()){
-      while(dishes_anti.empty()){
-        if(dishes_anti.top() > 0){
-          ans += dishes_anti.top();
-        }
-        else{
-          break;
-        }
-      }
-      break;
-    }
-    if(dishes_anti.empty()){
-      if(damage == 1){
-        break;
-      }
-      else{
-        ans += dishes_poison.top();
-        break;
-      }
-    }
-    a = dishes_poison.top();
-    b = dishes_anti.top();
-    if(damage == 0){
-      damage += 1;
-      ans += a;
-      dishes_poison.pop();
-      continue;
+    cin >> a >> b;
+    value.at(i).at(0) = a;
+    value.at(i).at(1) = b;
+  }
+  for(int i=0;i<N;i++){
+    if(value.at(i).at(0) == 1){
+      dp.at(i+1).at(1) = max(dp.at(i).at(1), dp.at(i).at(0)+value.at(i).at(1));
     }
     else{
-      if(a+b>0){
-        ans += b;
-        damage -= 1;
-        dishes_anti.pop();
-      }
-      else{
-        break;
-      }
+      dp.at(i+1).at(0) = max(dp.at(i).at(0),dp.at(i).at(1)+value.at(i).at(1))
     }
   }
-
   cout << ans << endl;
-  /*
-  for(int i=0;i<(int)dishes_poison.size();i++){
-    cout << dishes_poison.at(i) << endl;
-  }
-  */
 }
