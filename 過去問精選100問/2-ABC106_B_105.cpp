3fd81c2978acfcea65e7fae3380d2d4af949908d
RyotaST <omamesan0621@icloud.com>
2023-08-03 15:04:59 +0900

    O(√N)での解法

--- 過去問精選100問/2-ABC106_B_105.cpp
+++ 過去問精選100問/2-ABC106_B_105.cpp
@@ -6,15 +6,22 @@ int main(){
   cin >> N;
   int ans = 0;
   for(int i=1;i<=N;i++){
-    int y = 0;
     if(i%2 == 0){
       continue;
     }
-    for(int j=1;j<=i;j++){
+    int y = 0;
+    for(int j=1;j*j<=i;j++){
       if(i%j == 0){
-        y += 1;
+        if(j*j != i){
+        	y += 2;
+        }
+        else{
+          y+=1;
+        }
       }
-      if(y == 8) ans += 1;
+    }
+    if(y == 8){
+      ans += 1;
     }
   }
   cout << ans << endl;
