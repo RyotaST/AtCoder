4460a57bba346380afe3b18b56b57b2815cb90df
RyotaST <omamesan0621@icloud.com>
2023-08-12 22:58:18 +0900

    AC

---
+++ AtCoder_Beginner_Contest/ABC314/A-314.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,x,n) for (int i=(int)x;i<(int)n;i++)
+
+int main(){
+  int N;
+  cin >> N;
+  string s = "1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+  cout << "3."+s.substr(0,N) << endl;
+}
