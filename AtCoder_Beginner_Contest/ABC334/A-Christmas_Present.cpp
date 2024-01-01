1fca2b1dc92f66ed0e810467107d75b4df60e35a
RyotaST <omamesan0621@icloud.com>
2024-01-01 23:59:23 +0900

    AC

---
+++ AtCoder_Beginner_Contest/ABC334/A-Christmas_Present.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,x,n) for (int i=(int)x;i<(int)n;i++)
+
+int main(){
+  int B,G;
+  cin >> B >> G;
+  if(B > G){
+    cout << "Bat" << endl;
+  }
+  else{
+    cout << "Glove" << endl;
+  }
+}
