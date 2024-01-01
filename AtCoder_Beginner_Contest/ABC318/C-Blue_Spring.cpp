0b2b334ace27a5a075aa4b344aa8ad72c5f2eadb
RyotaST <omamesan0621@icloud.com>
2024-01-01 23:50:33 +0900

    途中

---
+++ AtCoder_Beginner_Contest/ABC318/C-Blue_Spring.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,x,n) for (int i=(int)x;i<(int)n;i++)
+
+int main(){
+  int N,D;
+  long long P;
+  cin >> N >> D >> P;
+  vector<long long> F(N);
+  long long day = 0;
+  vector<long long> dp;
+  rep(i,0,N) cin >> F[i];
+  
+}
