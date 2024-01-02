87071b495c9dc17f91b74f03c6801f968bbe94b2
RyotaST <omamesan0621@icloud.com>
2024-01-02 18:17:19 +0900

    途中

---
+++ AtCoder_Beginner_Contest/ABC334/B-Christmas_Trees.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,x,n) for (int i=(int)x;i<(int)n;i++)
+
+int main(){
+  long long A,L,R;
+  long long Left, Right, ans;
+  int M;
+  cin >> A >> M >> L >> R;
+  Left = abs(L-M);
+  Right = abs(R-M);
+}
